350aed06e7a14038373dc39733c4c3cca8f2464f
nicoGelato <86999857+NicoGelato@users.noreply.github.com>
2022-08-16 03:14:03 -0300

    Listar datos

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,24 +1,32 @@
 import { pool } from "../../../config/db";
 
+const setProducts = async (req, res) => {
+  const { name, description, price } = req.body;
+  const [result] = await pool.query("INSERT INTO products SET ?", {
+    name,
+    description,
+    price,
+  });
+
+  return res.status(200).json({
+    name,
+    price,
+    description,
+    id: result.insertId,
+  });
+};
+
+const getProducts = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM products");
+  return res.status(200).json(result);
+};
+
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      return res.status(200).json("Getting products");
-
+      return await getProducts(req, res);
     case "POST":
-      const { name, description, price } = req.body;
-      const [ result ] = await pool.query("INSERT INTO products SET ?", {
-        name,
-        description,
-        price,
-      });
-      console.log(result);
-      return res.status(200).json({
-        name,
-        price,
-        description,
-        id: result.insertId,
-      });
+      return await setProducts(req, res);
   }
 
   return res.status(200).json("Getting all products");
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
 import ProductForm from "../components/ProductForm";
+import Nav from "../components/Nav";
+import axios from "axios";
 
-export default function Home() {
+export const getServerSideProps = async (context) => {
+  const { data: products } = await axios.get(
+    "http://localhost:3000/api/products"
+  );
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+const Home = ({ products }) => {
+  console.table(products);
   return (
     <div>
+      <Nav />
       <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
         Home
       </h1>
       <ProductForm />
+      <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
+        Products{" "}
+      </h1>
+      {products.map(({id, name, price, description}) => (
+        <div key={id}>
+          <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600"> {name} </h2>  
+          <p className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600"> {description} </p>
+          <p className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600"> ${price} </p>
+
+        </div>
+      ))}
     </div>
   );
-}
+};
+export default Home;
